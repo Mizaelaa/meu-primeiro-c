7205c723fd74db0ca008c65c3402b6aab1b55285
mizaelaa <mizaelanunes@gmail.com>
2025-04-20 15:54:28 -0300

    Adicionando numero de tentativas

--- adivinhacao.c
+++ adivinhacao.c
@@ -6,21 +6,24 @@ int main (){
     int numerosecreto;
     numerosecreto =  42;
     int chute;
-    int acertou = chute == numerosecreto;
-
+    
+    for(int i = 1; i <=5; i++){
     printf("Qual o numero secreto?");
     scanf("%d", &chute);
+    int acertou = chute == numerosecreto;
 
     if(acertou){
         printf("Parabéns, você acertou!!");
+        break;
     }
     else{
         int maior = chute > numerosecreto;
         if(maior){
             printf("%d é maior que o numero secreto\n" , chute);
         } else {
-            printf("%d é menor que o numero secreto" , chute);
+            printf("%d é menor que o numero secreto\n" , chute);
         }
     }
+}
         
 }
